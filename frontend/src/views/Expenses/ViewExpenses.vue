88f7e6c7f79bbc3360c5b887ee23cdb92c1095d1
Mohamed Maahi <mohamedmaahi@gmail.com>
2025-12-27 13:08:18 +0530

    WIP: save local changes before merging origin/main

--- frontend/src/views/Expenses/ViewExpenses.vue
+++ frontend/src/views/Expenses/ViewExpenses.vue
@@ -41,8 +41,7 @@
         </div>
       </div>
       
-<<<<<<< HEAD
-=======
+
       <div class="bg-white p-4 rounded-lg shadow border dark:bg-slate-800 dark:border-slate-700 dark:text-white">
         <div class="flex items-center">
           <div class="bg-purple-100 p-3 rounded-lg mr-4 dark:bg-purple-900/20">
@@ -54,7 +53,7 @@
           </div>
         </div>
       </div>
->>>>>>> 296b82e6ae09dd8820fce6d2be42e34e86375736
+
       </div>
 
     <!-- Filters -->
